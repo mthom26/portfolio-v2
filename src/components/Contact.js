3778e4c248357ec8be68a9a7ff2c5e2b7791fafe
mthom26 <mthom2626@gmail.com>
2018-10-01 12:04:01 +0100

    Add Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './Contact.css';
+import emailIcon from '../images/envelopeWhite.svg';
+import githubIcon from '../images/githubWhite.svg';
+
 const Contact = ({ reference }) => {
   return (
-    <div ref={reference} id="contact" style={{height: '60vh'}}>
-      <h1>Contact</h1>
+    <div className="contact" ref={reference} id="contact">
+      <h2>Contact Me</h2>
+      <p>If you want to discuss your project just send me an email.</p>
+
+      <div className="contactItem">
+        <h3 className="contactSpan">Email</h3>
+        <a href="mailto:michaelthompson.code@gmail.com">
+          michaelthompson.code@gmail.com
+        </a>
+      </div>
+
+      <div className="contactItem">
+        <h3 className="contactSpan">Links</h3>
+        <a className="contactLink" href="mailto:michaelthompson.code@gmail.com">
+          <img src={emailIcon} width="32px" alt="" />
+        </a>
+        <a className="contactLink" href="https://github.com/mthom26">
+          <img src={githubIcon} width="32px" alt="" />
+        </a>
+      </div>
     </div>
   )
 };
