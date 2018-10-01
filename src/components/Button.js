2d9334764ed5ccc6058a31ecff36991a72006cd8
mthom26 <mthom2626@gmail.com>
2018-10-01 14:31:11 +0100

    Refactor Button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,13 +4,17 @@ import './Button.css';
 
 const Button = ({ text, icon, link }) => {
   return (
-    <a href={`${link}`} className="button">
-      <span className="buttonText">
-        {text}
-      </span>
-      <div className="buttonIcon">
-        <img src={icon} alt="" />
+    <a href={`${link}`} className="buttonBase">
+      <div className="button">
+        <span className="buttonText">
+          {text}
+        </span>
+        <div className="buttonIcon">
+          <img src={icon} alt="" />
+        </div>
       </div>
+      <div className="buttonBottom" />
+      <div className="buttonBottomVar" />
     </a>
   );
 };
